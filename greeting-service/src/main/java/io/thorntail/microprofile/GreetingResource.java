e1c513daec31dbe36c45409144d0945a2a4c2a6c
Ken Finnigan <ken@kenfinnigan.me>
2018-10-30 12:14:45 +0100

    Add MP spec usage

--- greeting-service/src/main/java/io/thorntail/microprofile/GreetingResource.java
+++ greeting-service/src/main/java/io/thorntail/microprofile/GreetingResource.java
@@ -1,12 +1,20 @@
 package io.thorntail.microprofile;
 
+import java.net.URL;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import io.opentracing.Tracer;
+import io.smallrye.opentracing.SmallRyeClientTracingFeature;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
 /**
  * @author Ken Finnigan
@@ -15,16 +23,31 @@ import org.eclipse.microprofile.faulttolerance.Fallback;
 @RequestScoped
 public class GreetingResource {
 
+    @Inject
+    @ConfigProperty(name = "name", defaultValue = "nobody")
+    private String name;
+
+    @Inject
+    Tracer tracer;
+
     @GET
     @Path("/greeting")
     @Produces("application/json")
+    @Timed
     @Fallback(fallbackMethod = "greetingFallback")
     public Response greeting() throws Exception {
+        NameService nameService = RestClientBuilder.newBuilder()
+                .baseUrl(new URL("http://localhost:8081"))
+                .register(new SmallRyeClientTracingFeature(tracer))
+                .build(NameService.class);
         return Response.ok()
-                .entity(new Greeting("Hello Voxxed!!"))
+                .entity(new Greeting("Hello " + nameService.getName()))
                 .build();
     }
 
+    private Response greetingFallback() {
+        return Response.ok().entity(new Greeting("Greeting Fallback")).build();
+    }
     static class Greeting {
         private final String content;
 
